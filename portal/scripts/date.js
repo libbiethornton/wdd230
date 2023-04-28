9de06a677f999b05e9cf17e4f8fcda63277c80b0
libbiethornton <tho19009@byui.edu>
2023-04-28 16:54:03 -0600

    revisions to portal page

--- portal/scripts/date.js
+++ portal/scripts/date.js
@@ -1,1 +1,5 @@
-let nLastModif = Date.parse(document.lastModified);
+let x = document.lastModified;
+document.getElementById("updated").innerHTML = "Last updated: " + x;
+
+let currentYear = new Date().getFullYear();
+document.querySelector("span").innerHTML = currentYear
