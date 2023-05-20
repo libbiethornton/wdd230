9911b3cfe7dd10bd4fcfdefca3bbc0895c74d9f8
libbiethornton <tho19009@byui.edu>
2023-05-20 14:47:29 -0600

    update homepage

--- portal/chamber/scripts/script.js
+++ portal/chamber/scripts/script.js
@@ -9,3 +9,12 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 document.querySelector("p").innerHTML = fulldate;
+
+function myFunction() {
+	var x = documet.getElementById("myLinks");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
