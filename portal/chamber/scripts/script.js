e46362e21cdf9f96b79436f0d9109f0eeb0d3f2f
libbiethornton <tho19009@byui.edu>
2023-06-14 14:52:51 -0600

    directory

--- portal/chamber/scripts/script.js
+++ portal/chamber/scripts/script.js
@@ -48,3 +48,47 @@ function displayDaysSinceLastVisit() {
 
   visitsDisplay.innerText = differenceInDays;
 }
+
+// cards for the directory page
+const url = 'https://libbiethornton.github.io/wdd230/portal/chamber/data.json';
+
+async function getCompanyData(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayCompanies(data.companies);
+};
+
+getCompanyData(url);
+
+const displayCompanies = (companies) => {
+  const cards = document.querySelector('div.cards');
+
+  companies.forEach((company) => {
+    let card = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let logo = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+
+    h3.textContent = `${company.business}`;
+    address.textContent = `${company.address}`;
+    phone.textContent = `${company.phone}`;
+    website.textContent = `${company.website}`;
+
+    logo.setAttribute('src', company.imageurl);
+    logo.setAttribute('alt', `${company.business} logo`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '340');
+
+    card.appendChild(h3);
+    card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+
+    cards.appendChild(card);
+
+  })
+};
+
